bf27fcdb4244e8232cf128f116d89397c70c9180
shinequasar <shinequasar@gmail.com>
2022-10-20 18:05:40 +0900

    [feat] 양방향 바인딩으로 폼에 남아있는 값 없애기

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,61 @@
 import './ExpenseForm.css'
+import {useState} from "react";
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
+    const submitHandler = (event)=>{
+      event.preventDefault(); // 페이지가 reloae 되는 것을 방지
+      const expenseData = {
+          title:enteredTitle,
+          amount:enteredAmount,
+          date: new Date(enteredDate)
+      };
+      console.log(expenseData);
+      setEnteredTitle('');
+      setEnteredAmount('');
+      setEnteredDate('');
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>항목이름</label>
-                    <input type='text'/>
+                    <input type='text'
+                           value={enteredTitle}
+                           onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>가격</label>
-                    <input type='number' min="0.01" step="0.01"/>
+                    <input type='number'
+                           min="0.01"
+                           step="0.01"
+                           value={enteredAmount}
+                           onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>날짜</label>
-                    <input type='date' min="2018-01-01" step="2023-12-31"/>
+                    <input type='date'
+                           min="2018-01-01"
+                           max="2023-12-31"
+                           value={enteredDate}
+                           onChange={dateChangeHandler}/>
                 </div>
                 <div className='new-expense__actions'>
-                    <button type='submit'>추가하기</button>
+                    <button type='submit' >추가하기</button>
                 </div>
             </div>
         </form>
